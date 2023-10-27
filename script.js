3997cf935bc2e9c14bce7eba9f623f88efd47632
unknown <roger.penhasenai@gmail.com>
2023-10-27 10:02:42 -0300

    animações,reformulação do formulário, interatividade da página alterados

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-var nav = document.getElementsByTagName('nav');
-function aparecer_apagar(){
-    if(nav.style.display === "none"){
-        nav.style.display = "block";
-    }else{
-        nav.style.display = "none";
-    }
+//script para mudar o display do menu
+function Mudarestado(el) {
+    var display = document.getElementById(el).style.display;
+    if (display == "none")
+        document.getElementById(el).style.display = 'block';
+    else
+        document.getElementById(el).style.display = 'none';
 }
+//script para mudar o display da lista em habilidades
+function Mudarestado2(el) {
+    var display = document.getElementById(el).style.display;
+    if (display == "none")
+        document.getElementById(el).style.display = 'block';
+    else
+        document.getElementById(el).style.display = 'none';
+}
+
+function validacao(el){
+    
+    }
